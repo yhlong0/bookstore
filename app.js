2902d3cd799483bf0eab92931e1e24a43dc39392
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2017-08-22 12:41:49 -0500

    Finish EJS template engine setup

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.render('index', { list:['a', 'b'] });
+	res.render('index', { title:'Hello', list:['a', 'b'] });
 });
 
 app.get('/books', function(req, res){
