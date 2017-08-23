d806e7686288b9048a53461179a48b905efb6e63
Hailong Yang <hailongyang@Hailongs-MBP-2.attlocal.net>
2017-08-22 23:17:39 -0500

    Finish Book routes, separate files, Single book route

--- app.js
+++ app.js
@@ -2,60 +2,12 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 3000;
-var bookRouter = express.Router();
+var bookRouter = require('./src/routes/bookRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
-
-var books = [
-	{
-		title: 'Hello1',
-		genre: 'Fiction',
-		author: 'William',
-		read: false
-	},
-	{
-		title: 'Hello2',
-		genre: 'Fiction2',
-		author: 'William2',
-		read: false
-	},
-	{
-		title: 'Hello3',
-		genre: 'Fiction3',
-		author: 'William3',
-		read: false
-	},
-	{
-		title: 'Hello4',
-		genre: 'Fiction4',
-		author: 'William4',
-		read: false
-	},
-	{
-		title: 'Hello5',
-		genre: 'Fiction5',
-		author: 'William5',
-		read: false
-	}
-];
-
-bookRouter.route('/')
-	.get(function(req, res){
-		res.render('books', 		
-		{ 
-			title: 'Book', 
-		  	nav: [{Link: '/Books', Text: 'Books'}, {Link: '/Authors', Text: 'Authors'}],
-		  	books: books
-		});
-	});
-
-bookRouter.route('/single')
-	.get(function(req, res){
-		res.send('Hello Single book');
-	});
 
 app.use('/Books', bookRouter);
 app.get('/', function(req, res){
@@ -67,11 +19,11 @@ app.get('/', function(req, res){
 	);
 });
 
-app.get('/books', function(req, res){
+app.get('/Books', function(req, res){
 	res.send('Hello books!');
 });
 
-app.get('/authors', function(req, res){
+app.get('/Authors', function(req, res){
 	res.send('Hello authors!');
 });
 
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,59 @@
+var express = require('express');
+
+var bookRouter = express.Router();
+
+var books = [
+	{
+		title: 'Hello1',
+		genre: 'Fiction',
+		author: 'William',
+		read: false
+	},
+	{
+		title: 'Hello2',
+		genre: 'Fiction2',
+		author: 'William2',
+		read: false
+	},
+	{
+		title: 'Hello3',
+		genre: 'Fiction3',
+		author: 'William3',
+		read: false
+	},
+	{
+		title: 'Hello4',
+		genre: 'Fiction4',
+		author: 'William4',
+		read: false
+	},
+	{
+		title: 'Hello5',
+		genre: 'Fiction5',
+		author: 'William5',
+		read: false
+	}
+];
+
+bookRouter.route('/')
+	.get(function(req, res){
+		res.render('bookListView', 		
+		{ 
+			title: 'Book', 
+		  	nav: [{Link: '/Books', Text: 'Books'}, {Link: '/Authors', Text: 'Authors'}],
+		  	books: books
+		});
+	});
+
+bookRouter.route('/:id')
+	.get(function(req, res){
+		var id = req.params.id
+		res.render('bookView',
+		{
+			title: 'Book',
+			nav: [{Link: '/Books', Text: 'Books'}, {Link: '/Authors', Text: 'Authors'}],
+			books: books[id]
+		});
+	});
+
+module.exports = bookRouter;
